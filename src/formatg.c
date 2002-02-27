b63eaeba8f487d70a9fd4590e4856dbc3c80f4d7
Brian Ripley <ripley@stats.ox.ac.uk>
2002-02-27 00:00:00 +0000

    version 3.1-9

--- src/formatg.c
+++ src/formatg.c
@@ -4,10 +4,27 @@
 */
 #include <stdio.h>
 #include "rpartS.h"
+#ifdef WIN32
+#include <ctype.h>
+#endif
 void formatg( Sint *n, double *x, char **format, char **out) {
-    int i;
+    int i, len;
+    char *p;
 
     for (i=0; i< *n; i++) {
 	sprintf(out[i], format[i], x[i]);
+#ifdef WIN32
+	/* change e+/-00n to e+/-0n etc */
+	p = out[i];
+	len = strlen(p);
+	if (tolower(p[len-5]) == 'e' &&
+	    (p[len-4] == '+' || p[len-4] == '-') &&
+	    p[len-3] == '0' &&
+	    isdigit(p[len-2]) && isdigit(p[len-1])) {
+	    p[len-3] = p[len-2];
+	    p[len-2] = p[len-1];
+	    p[len-1] = '\0';
 	}
+#endif
     }
+}
