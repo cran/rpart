6d0ec181b3009e202872f87c0b49f40fd74b7f0f
Brian Ripley <ripley@stats.ox.ac.uk>
2011-03-06 18:38:23 +0000

    version 3.1-49

--- src/branch.c
+++ src/branch.c
@@ -1,84 +1,76 @@
+/* SCCS  @(#)branch.c	1.5 06/06/01 */
 /*
- * Walk an observation 'one more split' down the tree.  If there are no
- *   more splits, return 0, otherwise return the address of the new node.
- * A return of zero also comes about if surrogates aren't being used, and I
- *   hit a missing value.
- *
- * tree :  the current node
- * obs  :  the observation number.  This will be negative iff the primary
- *           split is missing.
- *
- * This is the one routine that accesses the tree by observation number,
- *  without the mediation of the rp.sorts array.  For missing value
- *  information it thus has to look at X directly using a macro.
- */
+** Walk an observation 'one more split' down the tree.  If there are no
+**   more splits, return 0, otherwise return the address of the new node.
+** A return of zero also comes about if surrogates aren't being used, and I
+**   hit a missing value.
+*/
 #include "rpart.h"
 #include "node.h"
 #include "rpartproto.h"
 
-pNode
-branch(pNode tree, int obs)
-{
-    int j, dir;
-    int category;               /* for categorical variables */
-    pNode me;
-    pSplit tsplit;
-    double **xdata;
-
-    if (!tree->leftson) return NULL;
+struct node *branch(struct node *tree, int obs)
+    {
+    int i, j, dir;
+    struct node *me;
+    struct split *tsplit;
+    FLOAT **xdata;
+    Sint   **sorts;
+    if (tree->leftson ==0) return(0);
 
     me = tree;
     xdata = rp.xdata;
-
-   /*
-    * choose left or right son
-    *   this may use lots of surrogates before we're done
+    sorts = rp.sorts;
+    /*
+    ** choose left or right son
+    **   this may use lots of surrogates before we're done
     */
     tsplit = me->primary;
-    j = tsplit->var_num;
-    if (R_FINITE(xdata[j][obs])) {
-	if (rp.numcat[j] == 0) {        /* continuous */
-	    dir = (xdata[j][obs] < tsplit->spoint) ?
-		tsplit->csplit[0] : -tsplit->csplit[0];
-	    goto down;
-	} else {                /* categorical predictor */
-	    category = (int) xdata[j][obs];     /* factor predictor -- which
-						 * level? */
-	    dir = (tsplit->csplit)[category - 1];
-	    if (dir)
+    j= tsplit->var_num;
+    if (rp.numcat[j]==0) { /* continuous */
+	for (i=0; i<rp.n; i++) {
+	    if (sorts[j][i] == obs) {  /* found the match */
+		if (xdata[j][i] < tsplit->spoint)
+			dir =  tsplit->csplit[0];
+		   else dir = -tsplit->csplit[0];
 		goto down;
-	}
-    }
-    if (rp.usesurrogate == 0)
-	return NULL;
-   /*
-    * use the surrogates
-    */
-    for (tsplit = me->surrogate; tsplit; tsplit = tsplit->nextsplit) {
-	j = tsplit->var_num;
-	if (R_FINITE(xdata[j][obs])) {  /* not missing */
-	    if (rp.numcat[j] == 0) {
-		dir = (rp.xdata[j][obs] < tsplit->spoint) ?
-		    tsplit->csplit[0] : -tsplit->csplit[0];
-		goto down;
-	    } else {
-		category = (int) xdata[j][obs]; /* factor predictor -- which
-						 * level */
-		dir = (tsplit->csplit)[category - 1];
-		if (dir)
-		    goto down;
+		}
 	    }
 	}
-    }
+    else {
+	dir = (tsplit->csplit)[(int)xdata[j][obs] -1];
+	if (dir !=0) goto down;
+	}
 
-
-    if (rp.usesurrogate < 2)
-	return NULL;
+    if (rp.usesurrogate==0) return(0);
     /*
-     * split it by default
-     */
+    ** use the surrogates
+    */
+    for (tsplit= me->surrogate; tsplit !=0; tsplit= tsplit->nextsplit) {
+	j = tsplit->var_num;
+	if (rp.numcat[j]==0) {
+	    for (i=0; i<rp.n; i++) {
+		if (sorts[j][i] == obs) {
+		    if (xdata[j][i] < tsplit->spoint)
+			    dir =  tsplit->csplit[0];
+		       else dir = -tsplit->csplit[0];
+		    goto down;
+		    }
+		}
+	    }
+	else {
+	    dir = (tsplit->csplit)[(int)xdata[j][obs] -1];
+	    if (dir !=0) goto down;
+	    }
+	}
+
+
+    if (rp.usesurrogate <2) return(0);
+    /*
+    ** split it by default
+    */
     dir = me->lastsurrogate;
 
-down:
-    return (dir == LEFT) ? me->leftson : me->rightson;
-}
+down:if (dir==LEFT) return(me->leftson);
+     else           return(me->rightson);
+    }
